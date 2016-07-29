5dad66a2e01ce40e4546bbf144d1043824cfead1
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-07-29 15:56:07 -0300

    Update politicians.py

--- Agent-Based-Modelling/politicians.py
+++ Agent-Based-Modelling/politicians.py
@@ -1,15 +1,25 @@
 #ABM
 #Politicians
 from turtle import Turtle
+from numpy.random import normal
 
 class Politician:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.turtle = Turtle()
-        self.turtle.speed('fastest')
-        self.turtle.color('red')
+        self.turtle.color('black')
         self.turtle.penup()
         self.turtle.goto(x, y)
         self.turtle.pendown()
+        self.votes = 0
+        self.wins = 0
         
+    def readjust_after_election(self, win, winner):        
+        if win == True:
+            self.turtle.color('green')
+        else:
+            self.turtle.color('red')
+            self.x = winner.x*0.5 + self.x*0.5
+            self.y = winner.y*0.5 + self.y*0.5
+            self.turtle.goto(self.x, self.y)
